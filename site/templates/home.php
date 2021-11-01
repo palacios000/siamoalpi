e71e469a60e866f0e3c348d393ff88295aaf6794
giulio <notice@carburo.net>
2021-11-01 17:55:24 +0100

    home landing: risistemato ordine colonne/righe

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,6 +1,6 @@
 <?php require "inc/head.php" ?>
 </head>
-<body class='<?php echo $page->template ?> bg-verde-sa antialiased'>
+<body class='landing-home bg-verde-sa antialiased'>
 
 
 	<section class="relative">
@@ -44,50 +44,64 @@
 
 		<!-- navigation END -->
 
-		<div class="absolute top-0 w-full">
+		<div class="absolute top-0 w-full flex ">
 			<?php
 			$bgFolder = $config->urls->templates . "pictures/bg-landing/bg/" ; ?>
-			<!-- striscia A -->
-			<div class="h-97 flex  ">
-				<div id="a1" class="relative flex-auto">
+
+			<!-- colonna A -->
+			<div class=" w-1/5 ">
+				<div id="a1" class="h-97 relative flex-auto">
 					<img class="absolute bottom-0 left-0 pb-5 pr-8" src="<?php echo $bgFolder ?>2siamo-alpi.jpg" alt="archivio Garlaschelli">
 				</div>
-				<div id="a2" class="relative w-3/5 justify-center">
+				<div id="a2" class="relative flex-auto h-full">
+					<img class="absolute top-48 left-0 pl-12 w-2/3 " src="<?php echo $bgFolder ?>3-9siamo-alpi.jpg" alt="archivio Garlaschelli">
+					<img class="absolute bottom-80 left-0  pr-28"  src="<?php echo $bgFolder ?>1-5siamo-alpi.jpg" alt="archivio Garlaschelli">
+				</div>
+			</div>
+
+			<!-- colonna B - content -->
+			<div class=" w-3/5 ">
+				<div id="b1" class="h-97 relative justify-center">
 					<img class="absolute bottom-0 left-0 pb-32 pl-16 w-1/3" src="<?php echo $bgFolder ?>3siamo-alpi.jpg" alt="archivio Garlaschelli">
 					<img class="absolute top-0 left-1/3 h-1/4 pl-3" src="<?php echo $bgFolder ?>1siamo-alpi.jpg" alt="archivio Garlaschelli">
 					<img class="absolute bottom-0 right-0 pb-10 pr-9 w-2/5" src="<?php echo $bgFolder ?>4siamo-alpi.jpg" alt="archivio Garlaschelli">
 				</div>
-				<div id="a3" class="relative flex-auto">
+				<div class="flex justify-center">
+					<div id="b2" class="w-99  text-white">
+						<div class="landing-body">
+							<p class="text-8xl uppercase mb-30 font-serif">Apriamo i cassetti</p>
+							<h1>titolo h1</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo neque aliquid obcaecati, eius et eveniet, sed animi? Quas harum aspernatur, commodi ab fugit mollitia hic aliquam quos, impedit, dicta, consequatur.</p>
+							<p>prova Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><h1>titolo h1</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo neque aliquid obcaecati, eius et eveniet, sed animi? Quas harum aspernatur, commodi ab fugit mollitia hic aliquam quos, impedit, dicta, consequatur.</p>
+							<p>prova Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+			<!-- colonna C -->
+			<div class=" w-1/5 ">
+				<div id="c1" class="h-97 relative flex-auto">
 					<img class="absolute top-0 right-0 pt-44 pl-14" src="<?php echo $bgFolder ?>5siamo-alpi.jpg" alt="archivio Garlaschelli">
 				</div>
-				
+				<div id="c2" class="relative flex-auto h-full">
+					<img class="absolute top-0 right-0 pt-14 pl-28 " src="<?php echo $bgFolder ?>3-6siamo-alpi.jpg" alt="archivio Garlaschelli">
+					<img class="absolute top-96 right-0 pl-16 pr-14" src="<?php echo $bgFolder ?>1-9siamo-alpi.jpg" alt="archivio Garlaschelli">
+					<img class="absolute bottom-64 right-0 pl-30 " src="<?php echo $bgFolder ?>1-7siamo-alpi.jpg" alt="archivio Garlaschelli">
+				</div>
 			</div>
-
-			<!-- stiscia B - content -->
-			<div class="flex ">
-				<div id="b1" class="relative flex-auto">
-					<img class="absolute top-48 left-0 pl-12 w-2/3 " src="<?php echo $bgFolder ?>3-9siamo-alpi.jpg" alt="archivio Garlaschelli">
-					<img class="absolute top-48 left-0 pl-12 w-2/3 " src="<?php echo $bgFolder ?>3-9siamo-alpi.jpg" alt="archivio Garlaschelli">
-				</div>
-				<div id="b2" class="w-99 justify-center text-white">
-					<div class="landing-body">
-						<p class="text-8xl uppercase mb-30 font-serif">Apriamo i cassetti</p>
-						<h1>titolo h1</h1>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo neque aliquid obcaecati, eius et eveniet, sed animi? Quas harum aspernatur, commodi ab fugit mollitia hic aliquam quos, impedit, dicta, consequatur.</p>
-						<p>prova Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					</div>
-
-				</div>
-				<div id="b3" class="relative flex-auto">
-				</div>
-				
-			</div>
-
 		</div>
 		
 
