230830f9e3020b708c07d39d7d2aef4d276aeb12
giulio <notice@carburo.net>
2021-12-01 19:08:08 +0100

    okimportazione  funziona!

--- site/templates/gestionale_sirbec-query.php
+++ site/templates/gestionale_sirbec-query.php
@@ -1,15 +1,30 @@
 <?php 
 
+/* CAMPI ProcessWire
+    # gestionale_sirbec-query
+    |------------------|---------------------------|
+    |     PW field     |        descrizione        |
+    |------------------|---------------------------|
+    | title            |                           |
+    | codice_esportato | stringa di inizio ricerca |
+    | codice           | resumption token          |
+    | codice_textarea  | termini di ricerca        |
+    |------------------|---------------------------|
+    
+    # gestionale_sirbec-importazione-scheda
+    |--------------|---------------------------------|
+    |   PW field   |            XML syntax           |
+    |--------------|---------------------------------|
+    | title        | dcterms:alternative             |
+    | display_name | dcterms:spacial  da controllare |
+    | codice       | identifier                      |
+    | descrizione  | dc:subject                      |
+    | immagini     | pico:ojcet                      |
+    | link         | dcterms:isReferencedBy          |
+    |--------------|---------------------------------|
 
-/* CAMPI 
-- title
-- codice_esportato
-- codice (resumption token)
-- codice_textarea (termin id ricerca)
-
-
-http://www.oai.servizirl.it/oai/interfaccia.jsp?verb=ListRecords&metadataPrefix=pico&set=AFRLSUP
-http://www.oai.servizirl.it/oai/interfaccia.jsp?verb=ListRecords&resumptionToken=null%7Cnull%7CAFRLSUP%7Cpico%7C300%7C2021-11-29T10:39:04Z
+    http://www.oai.servizirl.it/oai/interfaccia.jsp?verb=ListRecords&metadataPrefix=pico&set=AFRLSUP
+    http://www.oai.servizirl.it/oai/interfaccia.jsp?verb=ListRecords&resumptionToken=null%7Cnull%7CAFRLSUP%7Cpico%7C300%7C2021-11-29T10:39:04Z
 
 */
 
@@ -33,12 +48,17 @@ define('DCTERMS', 'http://purl.org/dc/terms/');
 
 
 $token = $xml->ListRecords->resumptionToken;
-echo $token;
+$page->of(false);
+$page->set('codice', $token);
+$page->save();
 
+
+// termini da inserire nella ricerca
+$keywords =  trim(str_replace("\n", '|' , $page->codice_textarea));
 
 foreach ($xml->ListRecords->record as $records) {
-    $identifier =  $records->header->identifier;
-    $urlFoto = $records->metadata->children(PICO)->record->object; // url foto
+
+    // 1. inizia ad estrapolare i titoli e descrizioni, per poi cercare i termini di ricerca
 
     // titolo da esplodere
     $dcTitle = $records->metadata->children(PICO)->record->children(DC)->title;
@@ -55,20 +75,64 @@ foreach ($xml->ListRecords->record as $records) {
         $counter++;
     }
     //echo $finalTitle;
-
     $description = $records->metadata->children(PICO)->record->children(DCTERMS)->alternative;
     $subject = $records->metadata->children(PICO)->record->children(DC)->subject;
-    $collection = $records->metadata->children(PICO)->record->children(DCTERMS)->isPartOf; // extra info
 
-    // url scheda da esplodere
-    $isReferenced = $records->metadata->children(PICO)->record->children(DCTERMS)->isReferencedBy; 
-    $isReferencedExplode = explode(";", $isReferenced);
-    foreach ($isReferencedExplode as $key => $value) {
-        if (strstr($value, "URL=")) {
-            $urlExplode = explode("=", $value);
-            $urlRecord = $urlExplode[1];
-        }
-    }
+    // 2. li inserisco in un WireData in modo da poter fare una ricerca con la function di PW
+    $unicaStringa = $finalTitle . " " . $description . " " . $subject; 
+
+    // 3. cerco i miei termini di ricerca
+    if(preg_match("($keywords)", $unicaStringa) === 1) { 
+
+        //prelevo tutti gli altri dati
+       
+            $identifier =  $records->header->identifier;
+            $urlFoto = $records->metadata->children(PICO)->record->object; // url foto
+            $collection = $records->metadata->children(PICO)->record->children(DCTERMS)->isPartOf; // extra info
+
+            // url scheda da esplodere
+            $isReferenced = $records->metadata->children(PICO)->record->children(DCTERMS)->isReferencedBy; 
+            $isReferencedExplode = explode(";", $isReferenced);
+            foreach ($isReferencedExplode as $key => $value) {
+                if (strstr($value, "URL=")) {
+                    $urlExplode = explode("=", $value);
+                    $urlRecord = $urlExplode[1];
+                }
+            }
+
+        // inizio importazione
+            //check if appuntamento is already there
+            $alreadyImported = $pages->findOne("template=gestionale_sirbec-importazione-scheda, codice=$identifier")->title;
+            if (!$alreadyImported) {
+                $p = new Page();
+
+                //define template & parent
+                $p->template = "gestionale_sirbec-importazione-scheda";
+
+                $p->title = $sanitizer->text($finalTitle);
+                $p->name  = $sanitizer->pageName($p->title, true);
+                $p->parent = $page;
+
+                $p->display_name = $sanitizer->text($collection);
+                $p->descrizione = $sanitizer->text($subject);
+                $p->link = $sanitizer->url($urlRecord);
+
+                // codice univoco
+                $p->codice = $sanitizer->text($identifier);
+
+                // add images
+                $p->save();
+                if(strlen($urlFoto)){
+                    $p->immagini->add((string)$urlFoto);
+                } 
+
+                $p->save();
+                echo 'new page <a href="' . $p->editUrl . '" target="_blank">' . $p->path . '</a><br>';
+            }
+    } 
+
+
+
 }
 // die()
 /* 
