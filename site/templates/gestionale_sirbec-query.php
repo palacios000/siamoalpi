532887f2ae5b6ad133a8693734450257613e4fd3
giulio <notice@carburo.net>
2021-12-01 15:38:24 +0100

    ci sono riuscito!

--- site/templates/gestionale_sirbec-query.php
+++ site/templates/gestionale_sirbec-query.php
@@ -1,4 +1,6 @@
 <?php 
+
+
 /* CAMPI 
 - title
 - codice_esportato
@@ -23,78 +25,34 @@ $schedeSirbec = WireArray();
 
 // $xmlstr = file_get_contents('http://www.oai.servizirl.it/oai/interfaccia.jsp?verb=ListRecords&metadataPrefix=pico&set=AFRLSUP');
 $xmlstr = file_get_contents("test-sirbec.xml");
-// $xmlstr = simplexml_load_file("test-sirbec.xml");
+$xml = new SimpleXMLElement($xmlstr);
 
-
-$library = new SimpleXMLElement($xmlstr);
-
-// $schedeSirbec->add($items);
-
-//$sxe = new SimpleXMLElement($items); 
-
-foreach ($library as $records) {
-
-
-    foreach($records as $record) {
-        
-        echo $record->header->identifier . "<br>";
-        
-
-        $namespaces = $record->metadata->getNamespaces(true);
-        // print_r($namespaces);
-       
-         $pico = $record->metadata->pico;
-        // $pico2 = new SimpleXMLElement($pico);
-         print_r($pico);
-
-        // var_dump($record->metadata->pico);
-        echo $record->metadata->children->pico;
-        echo "<br>";
-
-
-        // $pico->registerXPathNamespace('dc', 'http://purl.org/pico/1.0/');
-        // $result = $pico->xpath('//dc:title');
-        // foreach ($result as $title)
-        //   {
-        //   echo $title . "<br>";
-        //   }
+define('PICO', 'http://purl.org/pico/1.0/');
+define('DC', 'http://purl.org/dc/elements/1.1/');
+define('DCTERMS', 'http://purl.org/dc/terms/');
 
 
 
-        // foreach($record->metadata->children($namespaces['dc']) as $child) {
-        //   var_dump($child) . "<br>";
-        // }
-
-        // // 
-        // // $xml = new SimpleXMLElement($r);
-        // $xml->registerXPathNamespace('e', 'http://www.webex.com/schemas/2002/06/service/event');
-
-        // foreach($xml->xpath('//e:event') as $event) {
-        //     $event->registerXPathNamespace('e', 'http://www.webex.com/schemas/2002/06/service/event');
-        //     var_export($event->xpath('//e:sessionKey'));
-        // }
-
-        // 
-
-        // echo $record->metadata['dcterms:alternative'] . "<br>";
-
-        /*
-
-
-$xml = new SimpleXMLElement($r);
-$xml->registerXPathNamespace('e', 'http://www.webex.com/schemas/2002/06/service/event');
-
-foreach($xml->xpath('//e:event') as $event) {
-    $event->registerXPathNamespace('e', 'http://www.webex.com/schemas/2002/06/service/event');
-    var_export($event->xpath('//e:sessionKey'));
-}
-
-        */
-    }
+foreach ($xml->ListRecords->record as $records) {
+    echo $records->header->identifier;
+    echo "<br>";
+    echo $records->metadata->children(PICO)->record->object; // url foto
+    echo "<br>";
+    echo $records->metadata->children(PICO)->record->children(DC)->title;
+    echo "<br>";
+    echo $records->metadata->children(PICO)->record->children(DCTERMS)->alternative;
+    echo "<br>";
+    echo $records->metadata->children(PICO)->record->children(DC)->subject;
+    echo "<br>";
+    echo $records->metadata->children(PICO)->record->children(DCTERMS)->isReferencedBy; // url scheda
+    echo "<br>";
+    echo $records->metadata->children(PICO)->record->children(DCTERMS)->isPartOf; // extra info
+    echo "<br>";
+    
+    echo "<hr>";
 
 }
-die();
-
+// die()
 /* 
  # guida ####################################################################
  
